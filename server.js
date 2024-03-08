bae4aeb400796d06de57dacdf3b5395b79db4a3b
Valyria <76444716+itsValyria@users.noreply.github.com>
2024-03-08 10:44:43 +0100

    server.js: added detail page

--- server.js
+++ server.js
@@ -33,6 +33,15 @@ app.post('/', function (request, response) {
   response.redirect(303, '/')
 })
 
+// Maak een GET route voor een detailpagina met een request parameter id
+app.get('/service/:id', function (request, response) {
+  // Gebruik de request parameter id en haal de juiste persoon uit de WHOIS API op
+  fetchJson('https://fdnd-agency.directus.app/items/dh_services' + request.params.id).then((apiData) => {
+    // Render service.ejs uit de views map en geef de opgehaalde data mee als variable, genaamd service
+    response.render('service', {service: apiData.data})
+  })
+})
+
 // Stel het poortnummer in waar express op moet gaan luisteren
 app.set('port', process.env.PORT || 8000)
 
